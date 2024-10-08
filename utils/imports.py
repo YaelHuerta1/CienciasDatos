f8b101bf5e998494ebfb9f43a8f09fb15f3a0ce3
YaelHuerta1 <yaelhuertagv5@gmail.com>
2024-10-08 15:15:25 -0600

    Se agrego la regresion logistica tanto como los heatmaps

--- utils/imports.py
+++ utils/imports.py
@@ -1,10 +1,13 @@
+import os
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-
-from utils.visuals import normalize_
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, confusion_matrix, classification_report
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def read_data(file_path):
@@ -33,3 +36,44 @@ def linear_regresion(data_normalized, data):
 
     print(f'Mean Squared Error: {mse}')
     print(f'R2 Score: {r2}')
+
+def logistic_regresion(data_normalized):
+    output_path = f'output/heatmaps/logic_heat/'
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    X = data_normalized[['mean radius', 'mean texture', 'mean perimeter', 'mean area']]
+    y = data_normalized['target']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=30)
+
+    scaler = StandardScaler()
+    X_train_Scaled = scaler.fit_transform(X_train)
+    X_test_Scaled = scaler.transform(X_test)
+
+    model = LogisticRegression()
+    model.fit(X_train_Scaled, y_train)
+
+    y_pred = model.predict(X_test_Scaled)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Precision: {accuracy}")
+
+    cm = confusion_matrix(y_test, y_pred)
+    print("Matriz de confusion:\n", cm)
+
+    print(f"Reporte de Clasificación:\n", classification_report(y_test, y_pred))
+
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False, xticklabels=['Clase 0', 'Clase 1'],
+                yticklabels=['Clase 0', 'Clase 1'])
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.title('Confusion Matrix - Logistic Regression')
+    plt.savefig(f'{output_path}logicRegresion_heatmap.png')
+    plt.close()
+
+
+def load_data_sets():
+    data = load_breast_cancer()
+    df = pd.DataFrame(data.data, columns=data.feature_names)
+    df['target'] = data.target
+    return df
